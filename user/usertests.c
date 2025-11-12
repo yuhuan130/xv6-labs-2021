29674f06e1b8c6cd15d26c35346d78b18a66075f
“Alex <alexchien130@gmail.com>
2025-11-12 14:06:42 +0800

    Setup

--- user/usertests.c
+++ user/usertests.c
@@ -234,9 +234,7 @@ copyinstr3(char *s)
 
 // See if the kernel refuses to read/write user memory that the
 // application doesn't have anymore, because it returned it.
-void
-rwsbrk()
-{
+void rwsbrk(char *s){
   int fd, n;
   
   uint64 a = (uint64) sbrk(8192);
